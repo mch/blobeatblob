bc7606589557d7f9203991b7a3163fafb289a194
Matt Hughes <matt.hughes@calgaryscientific.com>
2016-03-25 23:34:31 -0600

    Getting the window dimensions, except at start up... :/

--- main.elm
+++ main.elm
@@ -1,39 +1,53 @@
 import Graphics.Collage exposing (..)
 import Color
+import Window
+
 import Effects
 import Html
 import Signal
 import Task
 import StartApp exposing (start)
 
-type alias Model =
-  {}
 
+type alias Model =
+  { windowSize : (Int, Int) }
+
+    
 type Action
-  = Tick
+  = WindowSize (Int, Int)
+
     
 init : (Model, Effects.Effects Action)
 init =
-  ({}, Effects.none)
+  ({ windowSize = (800, 600) }, Effects.none)
 
+    
 update : Action -> Model -> (Model, Effects.Effects Action)
 update action model =
-  (model, Effects.none)
-    
+  case action of
+    WindowSize (wx, wy) -> ( { model | windowSize = (wx, wy) }, Effects.none)
+
+                           
 view : Signal.Address Action -> Model -> Html.Html
 view address model =
-  Html.text "Hello World"
-    
+  let
+    (wx, wy) = model.windowSize
+  in
+    Html.fromElement (collage wx wy [filled Color.red (circle 50)])
+
+      
 app =
     start { init = init
           , update = update
           , view = view
-          , inputs = []
+          , inputs = [Signal.map (\x -> WindowSize x) Window.dimensions]
           }
 
+      
 main =
     app.html
 
+       
 port tasks : Signal (Task.Task Effects.Never ())
 port tasks =
     app.tasks
