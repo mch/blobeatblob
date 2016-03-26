75ff74e72d2b2c3d0c071d508227af043d17b804
Matt Hughes <matt.hughes@calgaryscientific.com>
2016-03-26 00:09:34 -0600

    Simplifying tagging of input signals, storing blob position rather than mouse position.

--- main.elm
+++ main.elm
@@ -2,6 +2,7 @@ import Graphics.Collage exposing (..)
 import Color
 import Window
 import Mouse
+import Time
 
 import Effects
 import Html
@@ -12,19 +13,20 @@ import StartApp exposing (start)
 
 type alias Model =
   { windowSize : (Int, Int)
-  , mousePosition : (Int, Int)
+  , blobPosition : (Int, Int)
   }
 
     
 type Action
   = WindowSize (Int, Int)
   | MousePosition (Int, Int)
+  | Tick Float
 
     
 init : (Model, Effects.Effects Action)
 init =
   ({ windowSize = (800, 600)
-   , mousePosition = (0, 0)
+   , blobPosition = (0, 0)
    }
   , Effects.none
   )
@@ -34,31 +36,34 @@ update : Action -> Model -> (Model, Effects.Effects Action)
 update action model =
   case action of
     WindowSize (wx, wy) -> ( { model | windowSize = (wx, wy) }, Effects.none)
-    MousePosition mp -> ( updateMousePosition model mp, Effects.none)
+    MousePosition mp -> ( updateBlobPosition model mp, Effects.none)
+    Tick dt -> ( model, Effects.none)
 
 
-updateMousePosition model mousePosition =
+updateBlobPosition model mousePosition =
   let
     (wx, wy) = model.windowSize
     (mx, my) = mousePosition
+    (bx, by) = model.blobPosition
   in
-    { model | mousePosition = (mx - wx // 2, wy // 2 - my) }
+    { model | blobPosition = (mx - wx // 2, wy // 2 - my) }
 
       
 view : Signal.Address Action -> Model -> Html.Html
 view address model =
   let
     (wx, wy) = model.windowSize
-    (mx, my) = model.mousePosition
+    (bx, by) = model.blobPosition
     blob = filled Color.red (circle 50)
-    translatedBlob = move (toFloat mx, toFloat my) blob
+    translatedBlob = move (toFloat bx, toFloat by) blob
   in
     Html.fromElement (collage wx wy [translatedBlob])
 
 
 inputs =
-  [ Signal.map (\x -> WindowSize x) Window.dimensions
-  , Signal.map (\x -> MousePosition x) Mouse.position
+  [ Signal.map WindowSize Window.dimensions
+  , Signal.map MousePosition Mouse.position
+  , Signal.map Tick (Time.fps 30)
   ]
         
 app =
