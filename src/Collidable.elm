f000f4f7fca5c262e19bd6d2b46c54251aa8bdc5
Matt Hughes <matt.hughes@calgaryscientific.com>
2016-04-08 16:16:42 -0600

    Using extensible records for things that can collide. Seems to lose the constructor for the type alias, so had to add constructor functions.

--- src/Collidable.elm
+++ src/Collidable.elm
@@ -1,27 +1,29 @@
 module Collidable (..) where
 
-type alias Collidable = { position : (Float, Float), size : Float }
+type alias Collidable a =
+  { a | position : (Float, Float), size : Float }
 
--- Returns a list of pairs of objects that have collided
-detectCollisionsForCircles : List Collidable -> List ( Collidable, Collidable )
+-- Returns a list of pairs of objects that have collided. Maybe this
+-- could take a function that can immedietly determine what to do with
+-- these pairs to avoid additional list traversals later.
+detectCollisionsForCircles : List (Collidable a) -> List (Collidable a, Collidable a)
 detectCollisionsForCircles circles =
   let
     distance : ( Float, Float ) -> ( Float, Float ) -> Float
     distance ( x1, y1 ) ( x2, y2 ) =
       sqrt ((x1 - x2) ^ 2 + (y1 - y2) ^ 2)
 
-    didCollide : Collidable -> Collidable -> List (Collidable, Collidable)
+    didCollide : Collidable a -> Collidable a -> List ( Collidable a, Collidable a )
     didCollide c1 c2 =
       if distance c1.position c2.position <= c1.size + c2.size then
         [(c1, c2)]
       else
         []
 
-    checkPairs : List Collidable -> List (Collidable, Collidable)
+    checkPairs : List (Collidable a) -> List (Collidable a, Collidable a)
     checkPairs l =
       case l of
         h :: t -> List.append (List.concatMap (didCollide h) t) (checkPairs t)
         [] -> []
   in
     Debug.log "collisions" (checkPairs circles)
-
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -4,6 +4,24 @@ import ElmTest exposing (..)
 import Collidable exposing (..)
 import String
 
+type alias Thing = { name : String }
+type alias SimpleThing = { position : (Float, Float), size : Float }
+
+makeThing : (Float, Float) -> Float -> String -> Collidable Thing
+makeThing p s n =
+  { position = p, size = s, name = n }
+
+thing1 : Collidable Thing
+thing1 = makeThing (0, 0) 5 "Thing One"
+thing2 : Collidable Thing
+thing2 = makeThing (5, 0) 5 "Thing Two"
+
+collidable1 : Collidable {}
+collidable1 = { position = (1, 2), size = 5 }
+
+makeCollidable : (Float, Float) -> Float -> Collidable {}
+makeCollidable p r =
+  { position = p, size = r }
 
 all : Test
 all =
@@ -11,13 +29,16 @@ all =
         [ test "Empty list"
             (assertEqual [] (detectCollisionsForCircles []))
         , test "No collisions with self"
-            (assertEqual [] (detectCollisionsForCircles [Collidable (1, 2) 5]))
+            (assertEqual [] (detectCollisionsForCircles [collidable1]))
         , test "No collision"
-            (assertEqual [] (detectCollisionsForCircles [Collidable (0, 0) 5, Collidable (0, 11) 5]))
+            (assertEqual [] (detectCollisionsForCircles [makeCollidable (0, 0) 5, makeCollidable (0, 11) 5]))
         , test "Close collision"
-            (assertEqual [(Collidable (0, 0) 5, Collidable (0, 10) 5)]
-               (detectCollisionsForCircles [Collidable (0, 0) 5, Collidable (0, 10) 5]))
+            (assertEqual [(makeCollidable (0, 0) 5, makeCollidable (0, 10) 5)]
+               (detectCollisionsForCircles [makeCollidable (0, 0) 5, makeCollidable (0, 10) 5]))
         , test "Overlapping Collision"
-            (assertEqual [(Collidable (0, 0) 5, Collidable (0, 9) 5)]
-               (detectCollisionsForCircles [Collidable (0, 0) 5, Collidable (0, 9) 5]))
+            (assertEqual [(makeCollidable (0, 0) 5, makeCollidable (0, 9) 5)]
+               (detectCollisionsForCircles [makeCollidable (0, 0) 5, makeCollidable (0, 9) 5]))
+        , test "Extended records"
+            (assertEqual [(thing1, thing2)]
+               (detectCollisionsForCircles [thing1, thing2]))
         ]
