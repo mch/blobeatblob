46b5dfbcdf70d3e090426bac977eaa9ebcc939a0
Matt Hughes <matt.hughes@calgaryscientific.com>
2016-04-08 14:18:55 -0600

    Extracting new collision detection to a new module.

--- src/Main.elm
+++ src/Main.elm
@@ -127,32 +127,6 @@ updateBlobPositionWithKeys model keys =
     { model | blob = newBlob }
 
 
-type alias Collidable = { position : (Float, Float), size : Float }
-
--- Returns a list of pairs of objects that have collided
-detectCollisionsForCircles : List Collidable -> List ( Collidable, Collidable )
-detectCollisionsForCircles circles =
-  let
-    distance : ( Float, Float ) -> ( Float, Float ) -> Float
-    distance ( x1, y1 ) ( x2, y2 ) =
-      sqrt ((x1 - x2) ^ 2 + (y1 - y2) ^ 2)
-
-    didCollide : Collidable -> Collidable -> List (Collidable, Collidable)
-    didCollide c1 c2 =
-      if distance c1.position c2.position <= c1.size + c2.size then
-        [(c1, c2)]
-      else
-        []
-
-    checkPairs : List Collidable -> List (Collidable, Collidable)
-    checkPairs l =
-      case l of
-        h :: t -> List.append (List.concatMap (didCollide h) t) (checkPairs t)
-        [] -> []
-  in
-    Debug.log "collisions" (checkPairs circles)
-
-         
 detectCollisions : Model -> Model
 detectCollisions model =
   let
